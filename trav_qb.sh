757479587e38ff7d9c78e4b8a31713f2e151047d
dhhsawdad <a1420768522@gmail.com>
2023-08-07 11:00:35 +0800

    update .sh file

--- trav_qb.sh
+++ trav_qb.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-cd /tmp
 
 git clone https://github.com/dhhsawdad/nat_hole.git
-
+cd ./nat_hole
 nohup ./trav -o /tmp/external.port -D &
 
 # ./trav-bittorrent -w /tmp/external.port -c qbittorrent -u admin -p adminadmin -s 127.0.0.1:8080
